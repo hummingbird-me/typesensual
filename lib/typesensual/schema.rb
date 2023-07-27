f2da985ec7038e597fa39ea61506b483b2a07e59
Emma Lejeck <nuck@kitsu.io>
2023-07-26 19:23:00 -0700

    Allow duplication of schemas

--- lib/typesensual/schema.rb
+++ lib/typesensual/schema.rb
@@ -4,8 +4,21 @@ require 'typesensual/field'
 
 class Typesensual
   class Schema
+    # Duplicate fields from the original
+    def initialize_copy(original)
+      %w[
+        @fields
+        @token_separators
+        @symbols_to_index
+        @default_sorting_field
+        @enable_nested_fields
+      ].each do |var|
+        instance_variable_set(var, original.instance_variable_get(var).dup)
+      end
+    end
+
     def initialize(&block)
-      instance_eval(&block)
+      instance_eval(&block) unless block.nil?
     end
 
     def field(name, type: 'auto', locale: nil, facet: nil, index: nil, optional: nil)
